79e6e12f8f0679590723f07733884512c871158e
Stuart Phythian <phythian_1@hotmail.com>
2019-01-22 17:19:43 +0100

    Update cachematrix.R

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,37 @@
-## Put comments here that give an overall description of what your
-## functions do
-
-## Write a short comment describing this function
+## Ass 2 - Lexical scoping
+## caching the inverse of a matrix
+## similar programming to makeVector:
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m<-NULL
+    ## NULL as this is an undefined term (as opposed to NA which is logical)
+    set<-function(y) {
+        ## X and invX at global scope level using <<-
+        x<<-y
+        invX<<-NULL
+    }    
+    get<-function() x
+    setinvrs<-function(invrs) invX<<-invrse
+    getinvrs<-function() invX
+    list(set=set, get=get,
+         setinvrs=setinvrs,
+         getinvrs=getinvrs)
 }
 
-
-## Write a short comment describing this function
+## The following cacheSolve returns the inverse of a matrix A created with
+## the makeCacheMatrix function.
+## If the cached inverse is available, cacheSolve retrieves it, while if
+## not, it computes, caches (at the global level), and returns it.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    ## Return a matrix that is the inverse of 'x'
+    invX <- x$getinvrs()
+    if (!is.null(invX)) {
+        message("getting cached inverse matrix")
+        return(invX)
+    } else {
+        invX <- solve(x$get())
+        x$setinvrs(invX)
+        return(invX)
+    }
 }
